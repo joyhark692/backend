9a8ddca1ddff28a1beb131e0c847560d068f3fc8
Joy Hark <joyhark692@gmail.com>
2025-11-04 03:06:48 +0530

    user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,28 +1,13 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    fullName: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    createdOn: {
-      type: Date,
-      default: Date.now,
-    },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
   },
-  { versionKey: false } // removes "__v"
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+export default User;
