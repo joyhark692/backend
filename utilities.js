1d2823f9972d296c286293cdc34b69fc1db9cc9e
Joy Hark <joyhark692@gmail.com>
2025-11-05 18:03:25 +0530

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -5,10 +5,14 @@ export function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.status(401).json({ success: false, message: "Access Denied" });
+  if (!token) {
+    return res.status(401).json({ success: false, message: "Access Denied" });
+  }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ success: false, message: "Invalid Token" });
+    if (err) {
+      return res.status(403).json({ success: false, message: "Invalid Token" });
+    }
     req.user = user;
     next();
   });
